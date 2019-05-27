ea48086558805dfd88b4516564bbc069ae292e19
Saul-Mirone <xy610888871@gmail.com>
2019-05-27 10:29:21 +0800

    remove unused

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,45 +10,8 @@ import System.IO
 
 import Parser (parseToplevel)
 
-compile :: String -> String
-compile x =
-  case parseToplevel x of
-    Right y -> init $ intercalate "" $ map ((++ "\n\n") . show) y
-    Left e -> show e
-
-type FileBase = String
-
-type FileExt = String
-
-type FilePathInfo = (FileBase, FileExt)
-
-parseName :: String -> FilePathInfo
-parseName x =
-  let x' = reverse x
-      f = (/= '.')
-      l = init $ reverse $ dropWhile f x'
-      r = reverse $ takeWhile f x'
-   in (l, r)
-
-compileIO :: String -> ExceptT String IO (IO ())
-compileIO file = do
-  let (base, ext) = parseName file
-  if ext /= "mio"
-    then throwError "Input file must has an extension of \".mio\"."
-    else do
-      x <- liftIO $ readFile file
-      return $ writeFile (base ++ ".mips") $ compile x
-
-resultReporter :: Either String (IO ()) -> IO ()
-resultReporter (Left e) = putStrLn $ "Compile failed with exception:\n" ++ e
-resultReporter (Right a) = putStrLn "Compile Success." >>= const a
-
 runCompiler :: IO ()
-runCompiler = do
-  let run = runExceptT . compileIO . head
-  y <- getArgs
-  x <- run y
-  resultReporter x
+runCompiler = putStrLn "Hello"
 
 process :: String -> IO ()
 process line = do
